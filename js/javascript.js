2d4984121fe201a63d5a2fbcabcaad98622828e6
Giselitaa <116329266+Giselitaa@users.noreply.github.com>
2022-12-06 02:03:06 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function cambiarfondo1() { document.body.style.backgroundColor = "black";}
+
+function cambiarfondo2() {
+  document.body.style.backgroundColor = "pink";
+}
+function cartel() {alert("Autora: Gisel Torsevia reside en la ciudad de Rosario, Email:Giseltorsevia99@gmail.com ")}
